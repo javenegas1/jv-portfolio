3695a66a59dd9be15adb339b718c494d9032a262
Juan <venegas.a.juan37@gmail.com>
2022-11-08 18:04:39 -0500

    project tech summary

--- portfolio-main/src/components/Projects.js
+++ portfolio-main/src/components/Projects.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 //bootstrap
 import { CupFill, Controller, Globe, EggFried } from 'react-bootstrap-icons';
+import { FaJsSquare } from 'react-icons/fa';
+import { DiJsBadge, DiCss3, DiBootstrap, DiMongodb, DiReact, DiPostgresql, DiDjango, DiPython } from 'react-icons/di';
 import Carousel from 'react-bootstrap/Carousel';
 
 export default function Projects() {
@@ -22,10 +24,10 @@ export default function Projects() {
           <h2>HotPlate <EggFried></EggFried> </h2>
           <p>Project initiated to help freelance chefs! Feel free to visit this <a href='https://hotplate-proj.herokuapp.com/' target="_blank" rel="noopener noreferrer" >website here</a> or its <a href='https://github.com/javenegas1/hotplate' target="_blank" rel="noopener noreferrer" >Github page here!</a></p>
           <ul>
-            <li>Python3</li>
-            <li>Django</li>
-            <li>PostgresQL</li>
-            <li>Bulma</li>
+            <li><DiPython/></li>
+            <li><DiDjango/></li>
+            <li><DiPostgresql/></li>
+            <li><DiCss3/></li>
           </ul>
         </Carousel.Caption>
       </Carousel.Item>
@@ -40,10 +42,10 @@ export default function Projects() {
           <h2>BarHops <CupFill></CupFill> </h2>
           <p>This project was started to help you find the next beer you want to drink! Feel free to visit this <a href='https://magical-sherbet-8dd300.netlify.app/' target="_blank" rel="noopener noreferrer" >website here</a> or its <a href='https://github.com/javenegas1/brewery-api' target="_blank" rel="noopener noreferrer" >Github page here!</a></p>
           <ul>
-            <li>MongoDB</li>
-            <li>Express</li>
-            <li>React</li>
-            <li>Node</li>
+            <li><DiMongodb/></li>
+            <li><DiReact/></li>
+            <li><DiJsBadge/></li>
+            <li><DiBootstrap/></li>
           </ul>
         </Carousel.Caption>
       </Carousel.Item>
@@ -58,9 +60,9 @@ export default function Projects() {
           <h2>Bravado <Globe></Globe> </h2>
           <p>This project was developed alongside Sohaib Ali <a href='https://www.linkedin.com/in/sohaib-ali-swe/' target="_blank" rel="noopener noreferrer" >(LinkedIn here)</a> to provide a forum for those who want to share interview experiences. Feel free to visit this <a href='https://bravado-project.herokuapp.com/bravado/' target="_blank" rel="noopener noreferrer" >website here</a> or its <a href='https://github.com/javenegas1/Bravado' target="_blank" rel="noopener noreferrer" >Github page here!</a></p>
           <ul>
-            <li>MongoDB</li>
-            <li>Express</li>
-            <li>Node</li>
+            <li><DiMongodb/></li>
+            <li><DiJsBadge/></li>
+            <li><DiBootstrap/></li>
           </ul>
         </Carousel.Caption>
       </Carousel.Item>
@@ -75,7 +77,8 @@ export default function Projects() {
           <h2>Minesweeper <Controller></Controller> </h2>
           <p>This project was simply a fun one for me! My goal was to focus on the fundamentals: Object-oriented programming, function recursion, and DOM manipulation. Feel free to visit this <a href='https://javenegas1.github.io/Minesweeper/' target="_blank" rel="noopener noreferrer" >website here</a> or its <a href='https://github.com/javenegas1/Minesweeper' target="_blank" rel="noopener noreferrer" >Github page here!</a></p>
           <ul>
-            <li>Node</li>
+            <li><DiJsBadge/></li>
+            <li><DiCss3/></li>
           </ul>
         </Carousel.Caption>
       </Carousel.Item>
